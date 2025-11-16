5597eba8e57d9c1243a64d1e11c21de4a20e4dbf
Agustina Sambucetti <agustinasambucetti@Air-de-Agustina.fibertel.com.ar>
2025-11-16 18:35:48 -0300

    elaboracion de boton de busqueda y menu de navegacion

---
+++ js/index.js
@@ -0,0 +1,51 @@
+// requerimientos del boton de busqueda
+
+let buscador = document.querySelector(".buscador");
+let boton = document.querySelector(".lupita");
+let form = document.querySelector(".busqueda");
+
+boton.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    let valor = buscador.value
+
+    if (valor == "") {
+        alert("Este campo no puede estar vacío");
+    } else if (buscador.value.length < 3) {
+        alert("La búsqueda debe tener al menos 3 caracteres");
+    } else {
+        form.submit();
+    }
+})
+
+
+// api dinamica
+
+let url = "https://dummyjson.com/products/category-list";
+let lista = document.querySelector(".listaCategorias");
+
+fetch(url)
+    .then(function (response) {
+        return response.json()
+    })
+    .then(function (data) {
+        console.log(data);  //el array de categorias
+
+        let contenido = "";
+
+        for (let i = 0; i < data.length; i++) {
+
+            contenido += `<li>
+            <li><a href="./category.html?id=${data[i]}" class="categoria">${data[i]}</a></li>
+        </li>`
+
+            lista.innerHTML = contenido;
+
+        }
+
+        console.log(characters);
+        lista.innerHTML = characters;
+    })
+    .catch(function (error) {
+        console.log(error)
+    })
