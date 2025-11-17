e3253079fe6c4cacb6fca20a0874403000389890
pivancovich-tech <pivancovich@udesa.edu.ar>
2025-11-17 12:32:15 -0300

    Creacion de carpetas product.js y category.js. Realizacion de product.js

---
+++ js/product.js
@@ -0,0 +1,53 @@
+let productUrl = 'https://dummyjson.com/products/1'
+let imgProduct = document.querySelector(".imgFlynn")
+let nombreProduct = document.querySelector(".articulosProduct h2")
+let descripcionProduct = document.querySelector (".articulosProduct h3")
+let precioProduct = document.querySelector (".articulosProduct h4")
+let marcaProduct = document.querySelector (".articulosProduct p.marca")
+let categoriaProduct = document.querySelector (".articulosProduct p.categoria")
+let stockProdcut = document.querySelector (".articulosProduct p.disponible")
+let tagsProduct = document.querySelector (".articulosProduct p.tags")
+
+fetch (productUrl)
+    .then (function (response){
+        return response.json()
+    })
+    .then (function(data){
+        console.log(data);
+        // NO SE COMO HACER LA IMAGEN
+
+        nombreProduct.innerText = data.title;
+        descripcionProduct.innerText = data.description;
+        precioProduct.innerText = "$" + data.price
+
+        //en este caso es innerHTML porque si fuese innerText se perderia el strong de Marca, se pierde el estilo.
+        marcaProduct.innerHTML = "<strong>Marca:</strong>" + data.brand;
+
+
+        categoriaProduct.innerHTML = `<strong>Categoria:</strong> <a href="./category.html?category=${data.category}">${data.category}</a>`;
+
+        //Para el stock, debo aplicar la teoria de condicionales.
+        if (data.stock > 0){
+            stock.innerHTML = "<strong>Stock:</strong> Disponible";
+        }
+        else{
+            stock.innetHTML = "<strong>Stock:</strong> Sin Stock";
+        }
+
+        //TAGS
+        let productAllTags = "";
+
+        for (let i = 0; i < data.tags.length; i++) {
+            producAllTags += "#" + data.tags[i] + "";
+
+        }
+        tagsProduct.innerHTML = "<strong>" + productAllTags + "</strong>";
+
+    })
+
+    .catch(function(error){
+        console.log(error)
+    })
+
+
+    //REVIEWS
