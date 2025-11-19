0ffb472a70abde3740cb43de173d97389c5a7a15
Micaela Son <mica@MacBook-Pro-7.local>
2025-11-18 22:43:42 -0300

    funcionalidad category.js

--- js/category.js
+++ js/category.js
@@ -2,8 +2,9 @@ let query = location.search;
 let queryObj = new URLSearchParams(query);
 let categoriaElegida = queryObj.get("category");
 
-let categoryUrl = `https://dummyjson.com/products/category/${categoriaElegida}`
-let categoria = document.querySelector(".artGeneral");
+
+let categoryUrl = `https://dummyjson.com/products/category/${categoriaElegida}`;
+let artGeneral = document.querySelector(".artGeneral");
 
 
 fetch(categoryUrl)
@@ -15,21 +16,23 @@ fetch(categoryUrl)
 
         let categoryProducts = data.products;
         let categoryContenido = "";
+        categoryContenido += `<h2 class="tituloGeneral"> ${categoriaElegida} </h2>`
 
         for (let i = 0; i < categoryProducts.length; i++) {
-            categoryContenido +=
-                `<article class="articulos">
+            categoryContenido += `
+            <article class="articulos">
                 <img class="fotos" src="${categoryProducts[i].thumbnail}" alt="${categoryProducts[i].title}">
                 <h3>${categoryProducts[i].title}</h3>
                 <p>${categoryProducts[i].description}</p>
-                <p>${categoryProducts[i].price}</p>
+                <p>$${categoryProducts[i].price}</p>
                 <a class="detalles" href="./product.html?id=${categoryProducts[i].id}">VER DETALLES</a>
-                </article>`;
+                </article>
+            `;
         }
 
-        categoria.innerHTML = categoryContenido;
+        artGeneral.innerHTML = categoryContenido;
     })
 
     .catch(function (error) {
-        console.log(error)
+        console.log('El error es: ' + error);
     })
